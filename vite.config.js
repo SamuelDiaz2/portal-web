d105358dd9a9ddc09037d53c76c02d49359d292d
SamuelDiaz2 <samuel.diazf@uniagustiniana.edu.co>
2026-03-04 23:08:54 -0500

    iniciar desde otro homepage

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portal-web/' // Asegúrate de que esta ruta coincida con tu configuración de GitHub Pages
 });
